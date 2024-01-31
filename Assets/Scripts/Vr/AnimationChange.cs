1825b2892514a64d7f7da348a3668d865875d55c
wzni <zhenni126@126.com>
2024-01-31 20:19:25 +0800

    Initial commit

---
+++ Assets/Scripts/Vr/AnimationChange.cs
@@ -0,0 +1,80 @@
+/*
+This script is used to implement VR interaction(set the light strength to a low value using the controller button).
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AnimationChange : MonoBehaviour
+{
+    public int flag = 0;
+    public DisplayHint hint;
+    public Animator[] animators;
+    //float temp = 1f;
+
+    private void Awake()
+    {
+
+    }
+
+
+    void Start()
+    {
+        
+
+    }
+
+    void Update()
+    {
+        //if(temp > 0.0f)
+        //{
+        //    temp -= 0.1f;
+        //    if (temp <= 0.0f)
+        //    {
+        //        ToggleByMenu();
+        //    }
+        //}
+
+    }
+
+    public void ToggleByMenu()
+    {
+
+        flag++;
+        if (flag == 2)
+        {
+            flag = 0;
+        }
+
+        if (flag == 0)
+        {
+            AnimationOn();
+        }
+        else if (flag == 1)
+        {
+            AnimationOff();
+        }
+
+        hint.Show("Animation Change");
+    }
+
+    public void AnimationOn()
+    {
+        foreach (Animator anim in animators)
+        {
+            anim.enabled = true;
+        }
+
+    }
+
+    public void AnimationOff()
+    {
+        foreach (Animator anim in animators)
+        {
+            anim.enabled = false;
+        }
+
+    }
+}
